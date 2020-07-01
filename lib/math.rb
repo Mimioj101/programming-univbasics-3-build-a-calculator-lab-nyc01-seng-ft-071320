586c03d3d9a1be267f655ff354a790e77f6a0fe4
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 17:28:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,11 +9,13 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-
+  divide = num1/num2
+  divide
 end
 
 def multiplication(num1, num2)
-
+  multiply = num1 * num2
+  multiply
 end
 
 def modulo(num1, num2)
